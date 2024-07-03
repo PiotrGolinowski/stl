ed2b5dc30ecca8cf3eb4a0fa05315effcdd48a98
PiotrGolinowski <120470701+PiotrGolinowski@users.noreply.github.com>
2024-07-03 18:29:52 +0200

    Homework containers: length-sort

--- homework/length-sort/main.cpp
+++ homework/length-sort/main.cpp
@@ -1,15 +1,35 @@
-#include <forward_list>
+#include <forward_list>https://github.com/coders-school/stl/blob/length-sort/homework/length-sort/main.cpp
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 #include "sort.hpp"
 
-template <typename Container>
-void printCollection(const Container& collection) {
-    for (const auto& element : collection) {
-        std::cout << element << ' ';
+bool compare_char(unsigned char x, unsigned char y)
+{
+    return tolower(x) < tolower(y);
+}
+
+
+bool compare_string(std::string input1, std:: string input2)
+{
+    if (input1.size() == input2.size())
+        return std::lexicographical_compare(input1.begin(),input1.end(),input2.begin(),input2.end(), compare_char);
+
+    return input1.size() < input2.size();
+}
+
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> input)
+{
+    std::deque<std::string> output {};
+    input.sort(compare_string);
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+    {
+        output.push_back(*it);
+
     }
-    std::cout << '\n';
+    return output;
 }
 
 int main() {
